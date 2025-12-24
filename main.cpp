663ab1f7ccd83f838792edcfcfe3de7726f40bc7
hayd <haydshafqat2@gmail.com>
2025-12-24 13:31:13 +0500

    slightly more updated

--- main.cpp
+++ main.cpp
@@ -3,9 +3,18 @@
 // TOD0: FILL mcqs.h with about 30/40 mcqs in that specific format
 
 #include <iostream>
+#include <cstdlib> // for random
+#include <ctime> // seeding random
+#include <cctype> // for toupper
 using namespace std;
 
 int main(){
+    int score = 0;
+    srand(time(0));
+    string contestent;
+
+    cout<<"Enter contestent name: ";
+    getline(cin,contestent);
     
     // TOD0:  BASIC  DISPLAY 
     // TOD0 : Modes(trivia , iq , typing) (optional)
@@ -13,21 +22,60 @@ int main(){
     // TOD0 : DISPLAY Instructions
     
     // TOD0: 3 chances (lives)
-    //LOOP 
+    
+    int chances = 3;
+    while (chances > 0 ){
+        //LOOP 
+        char answer;
+        int index = rand()%3;
 
         // TOD0 : Dispalying RANDOM Questions
-        // TOD0 : Dispalying Their options
-        // TOD0 : Matching  options
-        // TOD0 : Scoring
-        // TOD0 : Updating Score to Another file
-        
-    //
-    cout<<"A. ";
-    cout<<questions[0].options[0];
-    cout<<endl;
-    cout<<"B. ";
-    cout<<questions[0].options[1];
-    cout<<endl;
+        cout<<questions[index].question <<endl;
 
+        // TOD0 : Dispalying Their options
+        
+        cout<<"A. ";
+        cout<<questions[index].options[0];
+        cout<<endl;
+        cout<<"B. ";
+        cout<<questions[index].options[1];
+        cout<<endl;
+        cout<<"C. ";
+        cout<<questions[index].options[2];
+        cout<<endl;
+        cout<<"D. ";
+        cout<<questions[index].options[3];
+        cout<<endl;
+
+        // TOD0 : Matching  options
+
+        cout<<"Enter Your Answer : ";
+        cin>>answer;
+
+        if(toupper(answer) == questions[index].ans){
+            score++;
+            cout<<" Correct! "<<endl;
+
+        }
+        else{
+            chances--;
+            cout<<"Incorrect. Remaining Lives: "<<chances<<endl;
+        }
+
+
+
+
+      
+        
+        //
+        
+        
+    }
+    
+    cout<<endl<<endl;
+    cout<<"Contestent's Name: "<<contestent;
+    cout<<endl<<"Your score is : "<<score;
+    
+    // TOD0 : Updating Score to Another file
   
 }
--- mcqs.h
+++ mcqs.h
@@ -13,5 +13,5 @@ mcq questions[] = {
     
     { "All roses are flowers. Some flowers fade quickly. Which statement is true?", {"All roses fade","Some roses fade","Some flowers may fade","No roses fade"}, 'C' },
 
-    { "2 → 6, 3 → 12, 4 → 20. Then 5 → ?", {"25","30","35","40"}, 'B' }
+    { "2 -> 6 , 3 -> 12, 4 -> 20. Then 5 -> ?", {"25","30","35","40"}, 'B' }
 };
